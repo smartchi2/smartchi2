0650d098588d26c426c10e1ce949bfa31e67d9cf
sunday smart <165800864+smartchi2@users.noreply.github.com>
2024-04-06 06:59:28 -0700

    Add files via upload

---
+++ score.py
@@ -0,0 +1,8 @@
+
+name = str(input("Enter your name to continue: "))
+score = int(input("Enter your score: "))
+passmark = 45
+if score >= passmark:
+	print("congrate!!! you have passed and the score  is", score,  name)
+else:
+	print("bros you no pass oo! your score is", score )
