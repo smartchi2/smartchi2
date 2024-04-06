abc27468a6000fdf576e63ec95237231ee10de85
sunday smart <165800864+smartchi2@users.noreply.github.com>
2024-04-06 07:01:01 -0700

    Add files via upload

---
+++ number.py
@@ -0,0 +1,3 @@
+num = int(input("Enter number: "))
+new = ord("number")
+print(new)
---
+++ number1.py
@@ -0,0 +1,12 @@
+number1 = int(input('Enter first number'))
+number2 = int(input('Enter second number'))
+
+
+print('The addition of', number1 , "and", number2, 'is =', number1 + number2,
+'\nthe multiplication of', number1, 'and', number2, 'is =', number1 * number2,
+'\nthe subtraction of', number1, 'and', number2, 'is =', number1 - number2,
+'\nthe exponentiation', number1, 'and', number2, 'is =', number1 ** number2,
+'\nthe true division ', number1, 'and', number2, 'is =', number1 / number2,
+'\nthe floor division', number1, 'and', number2, 'is =', number1 // number2,
+'\nthe modulo', number1 ,'and', number2, 'is =', number1 % number2,
+'\nthe square root of', number1, 'and', number2, 'is =', number1 **(1/2), 'and', number2 ** (1/2))
