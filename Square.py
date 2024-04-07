a498e34d3bf7433d1b4c5d6580929b080d177773
sunday smart <165800864+smartchi2@users.noreply.github.com>
2024-04-07 00:13:24 -0700

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,6 @@
+print('number\t\tsquare\t\tcube')
+for num in range(6):
+ square = num **2
+ cube = num **3
+
+print(f"{num}\t\t{square}\t\t{cube}")
---
+++ grade2.py
@@ -0,0 +1,14 @@
+score = int(input("Enter grade: "))
+grade = 100
+if score <= grade:
+  print("your score is",score, "your grade is A")  
+elif grade > 65 and score <= 79:
+    print("your score is",score, "your grade is B")
+elif grade > 50 and score <= 64:
+    print("your score is",score, "your grade is C")
+elif grade > 40 and score <= 49:
+    print("your score is",score, "your grade is D")
+elif grade > 100:
+    print("you are a criminal your scores dey fear us")
+else:
+   print("you are a criminal your scores dey fear us")
