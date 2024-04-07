c7b9085ac8b5d7094fe9a7bf46667d41087b0503
sunday smart <165800864+smartchi2@users.noreply.github.com>
2024-04-06 23:56:52 -0700

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,8 @@
+grade = int(input("Enter grade:"))
+score = 90
+if score > 90:
+     print("congratulation! you passed with the grade of", grade)
+else:
+     print("Failed!! your grade is", grade)
+
+
