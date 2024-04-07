9e6c8dc360b04f4a486605a15f5cf86336075d47
sunday smart <165800864+smartchi2@users.noreply.github.com>
2024-04-07 00:15:23 -0700

    Add files via upload

---
+++ doneinclass.py
@@ -0,0 +1,29 @@
+number = 0
+number1 = 1
+number2 = 2
+number3 = 3
+number4 = 4
+number5 = 5
+ 
+square = number ** 2
+square1 = number1 ** 2
+square2 = number2 ** 2
+square3 = number3 ** 2
+square4 = number4 ** 2
+square5 = number5 ** 2
+
+cube = number ** 3
+cube1 = number1 ** 3
+cube2 = number2 ** 3
+cube3 = number3 ** 3
+cube4 = number4 ** 3
+cube5 = number5 ** 3
+
+print("number\t" "square\t" "cube")
+
+print(f"{number}\t    {square}\t     {cube}")
+print(f"{number1}\t   {square1}\t    {cube1}")
+print(f"{number2}\t   {square2}\t    {cube2}")
+print(f"{number3}\t   {square3}\t    {cube3}")
+print(f"{number4}\t   {square4}\t    {cube4}")
+print(f"{number5}\t   {square5}\t    {cube5}")
