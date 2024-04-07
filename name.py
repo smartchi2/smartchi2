1b3e0256a75d6fa772508fc18342d22ce8652d63
sunday smart <165800864+smartchi2@users.noreply.github.com>
2024-04-07 00:01:10 -0700

    Add files via upload

---
+++ name.py
@@ -0,0 +1,17 @@
+age = '14'
+name = 'lambo'
+message = "my name is " + name + ',' +   "I'm"  + age +  "years old"
+print(message)
+
+number1 = 15
+number2 = 10
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 // number2)
+print(number1 % number2)
+print(number1 ** number2)
+print(number1 **0.5)
+
